d82922314c095c6179f01d63ad525dee88ecf853
Antonio Guzman <aguzmanv09@gmail.com>
2020-08-14 17:32:53 -0500

    Creando la sección acerca de mi

--- src/frontend/pages/HomePage.tsx
+++ src/frontend/pages/HomePage.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Hero from '../components/Hero/Hero';
+import AboutMe from '../components/AboutMe/AboutMe';
 
 const HomePage = () => {
   return (
     <>
       <Hero />
+      <AboutMe />
       <h1>HomePage</h1>
     </>
   );
