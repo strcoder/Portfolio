e6eb353791a890a51af7dad6fc359fdee25dfc1c
Antonio Guzman <aguzmanv09@gmail.com>
2020-08-14 20:08:54 -0500

    Creando el contenedor para mostrar un proyecto

--- src/frontend/pages/HomePage.tsx
+++ src/frontend/pages/HomePage.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Hero from '../components/Hero/Hero';
 import AboutMe from '../components/AboutMe/AboutMe';
+import Project from '../components/Project/Project';
 
 const HomePage = () => {
   return (
     <>
       <Hero />
       <AboutMe />
+      <Project />
       <h1>HomePage</h1>
     </>
   );
